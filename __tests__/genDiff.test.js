7da1de475d68dd987e29311a82b25fd9efab3bc9
Slava Kostenko <shegan782@gmail.com>
2020-10-22 04:32:43 +0300

    d

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+
 import { expect } from '@jest/globals';
 import path from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index';
-import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,7 +13,6 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), { encoding: 'utf-8' });
 test('main flow', () => {
   const jsonComparingResult = readFile('jsonComparingResult.txt').toString().trim();
-  console.log(jsonComparingResult)
   const before = getFixturePath('before.json');
   const after = getFixturePath('after.json');
   const result = genDiff(before, after);
