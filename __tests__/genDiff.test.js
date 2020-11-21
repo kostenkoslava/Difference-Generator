88fc2826afe42663b62e54cf0ecc472999d99f9a
Slava Kostenko <shegan782@gmail.com>
2020-11-21 06:14:51 +0200

    refactoring #3

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -14,24 +14,24 @@ const getFixtureFile = (name, extension) => getFixturePath(`${name}.${extension}
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), { encoding: 'utf-8' });
 
 describe('comparing two configuration files', () => {
-	const expectedResult = {
-		stylish: readFile('stylish.txt'),
-		plain: readFile('plain.txt'),
-		json: readFile('json.txt'),
-	};
-	test.each(extensions)('%s files', (extension) => {
-		expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension)))
-			.toEqual(expectedResult.stylish);
-		expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension), 'plain'))
-			.toEqual(expectedResult.plain);
-		expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension), 'json'))
-			.toEqual(expectedResult.json);
-	});
-	test.each(extensions)('is valid JSON output (%s files)', (extension) => {
-		expect(() => JSON.parse(genDiff(
-			getFixtureFile('file1', extension),
-			getFixtureFile('file2', extension),
-			'json',
-		))).not.toThrow();
-	});
+  const expectedResult = {
+    stylish: readFile('stylish.txt'),
+    plain: readFile('plain.txt'),
+    json: readFile('json.txt'),
+  };
+  test.each(extensions)('%s files', (extension) => {
+    expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension)))
+      .toEqual(expectedResult.stylish);
+    expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension), 'plain'))
+      .toEqual(expectedResult.plain);
+    expect(genDiff(getFixtureFile('file1', extension), getFixtureFile('file2', extension), 'json'))
+      .toEqual(expectedResult.json);
+  });
+  test.each(extensions)('is valid JSON output (%s files)', (extension) => {
+    expect(() => JSON.parse(genDiff(
+      getFixtureFile('file1', extension),
+      getFixtureFile('file2', extension),
+      'json',
+    ))).not.toThrow();
+  });
 });
